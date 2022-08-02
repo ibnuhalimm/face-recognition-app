a7b59291058d14129f38707babd7a72b1c681ced
Ibnu Halim Mustofa <ibnuhalim@pm.me>
2022-08-02 11:35:49 +0700

    remove create_model method

--- recognition.py
+++ recognition.py
@@ -16,6 +16,9 @@ class Recognition:
             "Mustofa"
         ])
 
+        self.img_width = 128
+        self.img_height = 128
+
         self.model_file = os.path.join(os.getcwd(), 'model.h5')
         self.model = tf.keras.models.load_model(self.model_file)
 
@@ -33,37 +36,10 @@ class Recognition:
 
     def get_name(self):
         img = image.load_img(self.photo_image,
-                             target_size=(128, 128))
+                             target_size=(self.img_width, self.img_height))
         image_to_predict = np.expand_dims(img, axis=0)
         self.predictions = self.model.predict(image_to_predict)
         class_index = np.argmax(self.predictions, axis=1)
 
         return self.names[class_index][0]
-
-    def create_model(self):
-        model = Sequential()
-
-        model.add(Conv2D(filters=16,
-                         kernel_size=3,
-                         padding="same",
-                         activation="relu",
-                         input_shape=(128, 128, 3)))
-        model.add(MaxPool2D(pool_size=(2, 2)))
-
-        model.add(Conv2D(filters=32,
-                         kernel_size=3,
-                         padding="same",
-                         activation="relu"))
-        model.add(MaxPool2D(pool_size=(2, 2)))
-
-        model.add(Flatten())
-        # model.add(Dropout(rate=0.2))
-
-        model.add(Dense(units=64,
-                        activation="relu"))
-
-        model.add(Dense(units=len(self.names),
-                        activation="softmax"))
-
-        return model
 
